5439553c4ed1c0ebacf97f92b88a7839cb2708fe
kamil Çalışkan <37404388+KamilCaliskan@users.noreply.github.com>
2023-10-30 12:07:54 +0300

    Create beginner.java

---
+++ beginner.java
@@ -0,0 +1,12 @@
+public satatic void main(String[] args){
+
+
+  int x=10;
+  int y;
+  it abc;
+  abc=32;
+  System.out.println("Merhaba Dünya"+ abc);
+  abc=35;
+  System.out.println("Meraba Dünya"+abc);
+  String s="java baslangic";
+}
