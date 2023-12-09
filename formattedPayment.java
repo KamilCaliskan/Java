a31e3b9d2c4dfd6d24faed53f094f782c9db31d0
kamil Çalışkan <37404388+KamilCaliskan@users.noreply.github.com>
2023-12-09 15:00:56 +0300

    formattedPayment.java

---
+++ formattedPayment.java
@@ -0,0 +1,41 @@
+/*
+Given a double-precision number, payment, denoting an amount of money,
+use the NumberFormat class' getCurrencyInstance method to convert
+
+into the US, Indian, Chinese, and French currency formats
+Then print the formatted values as follows:
+
+US: formattedPayment
+India: formattedPayment
+China: formattedPayment
+France: formattedPayment
+*/
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // Create NumberFormat objects for US, India, China, and France locales
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        // Format the payment for each locale
+        String us = usFormat.format(payment);
+        String india = indiaFormat.format(payment);
+        String china = chinaFormat.format(payment);
+        String france = franceFormat.format(payment);
+        
+        // Print the formatted values
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
