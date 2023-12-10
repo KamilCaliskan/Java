6a1f1cb01ef6592a2aee5ae8982f9e710e10b7a4
kamil Çalışkan <37404388+KamilCaliskan@users.noreply.github.com>
2023-12-10 22:40:19 +0300

    Update StringProblem.java

--- StringProblem.java
+++ StringProblem.java
@@ -1,7 +1,36 @@
 /* 
-Bu Java programı, bir tamsayıyı bir dizeye
-dönüştürmeyi ve ardından bu dizeyi tekrar tamsayıya dönüştürerek kullanıcının girdiği 
-sayının doğru bir şekilde işlendiğini kontrol etmeyi amaçlar
+Bu Java programı, bir tamsayıyı bir dizeye dönüştürmeyi 
+ve ardından bu dizeyi tekrar tamsayıya dönüştürerek kullanıcının girdiği sayının 
+doğru bir şekilde işlendiğini kontrol etmeyi amaçlar
+
+Programın genel akışı şu şekildedir:
+
+    Kullanıcıdan bir tamsayı alınır (n).
+    Bu tamsayı, Integer.toString(n) kullanılarak bir dizeye dönüştürülür ve s adlı değişkene atanır
+    Ardından, bu dize tekrar tamsayıya dönüştürülerek orijinal tamsayı ile karşılaştırılır (n == Integer.parseInt(s))
+    Eğer dönüşümler başarılıysa, yani kullanıcının girdiği sayı ile dizeye dönüştürülen 
+    ve tekrar tamsayıya dönüştürülen sayı aynı ise, "Good job" yazdırılır. Aksi takdirde "Wrong answer." yazdırılır.
+    Eğer program beklenmeyen bir şekilde sonlandırılmaya çalışılırsa, 
+    yasaklanmış bir çıkış kullanıldığını belirten bir mesaj ("Unsuccessful Termination!!") yazdırılır.
+
+Programın temel amacı, tamsayıyı dizeye dönüştürme ve bu dizeyi 
+tekrar tamsayıya dönüştürme işlemlerini doğru bir şekilde gerçekleştirebilmektir.
+
+DoNotTerminate sınıfının içinde, programın çıkışını kontrol etmek amacıyla bir güvenlik yöneticisi 
+(SecurityManager) oluşturulmuştur
+Bu yönetici, exitVM izni ile ilgili bir istisna fırlatır
+
+public static void forbidExit() {
+    final SecurityManager securityManager = new SecurityManager() {
+        @Override
+        public void checkPermission(Permission permission) {
+            if (permission.getName().contains("exitVM")) {
+                throw new ExitTrappedException();
+            }
+        }
+    };
+    System.setSecurityManager(securityManager);
+}
 */
 
 import java.util.*;
