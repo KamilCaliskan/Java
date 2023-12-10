43fbda640db2a08b16b04d1a52625da99075c7d5
kamil Çalışkan <37404388+KamilCaliskan@users.noreply.github.com>
2023-12-10 21:57:00 +0300

    StringProblem.java

---
+++ StringProblem.java
@@ -0,0 +1,53 @@
+/* 
+Bu Java programı, bir tamsayıyı bir dizeye
+dönüştürmeyi ve ardından bu dizeyi tekrar tamsayıya dönüştürerek kullanıcının girdiği 
+sayının doğru bir şekilde işlendiğini kontrol etmeyi amaçlar
+*/
+
+import java.util.*;
+import java.security.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            String s = Integer.toString(n); // Convert integer to string
+
+            
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
+
+// The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
