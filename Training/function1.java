eea7b5501abf848e60896a983b4edaccc5584bd1
Kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-06-18 18:14:58 +0200

    Create function1.java

---
+++ Training/function1.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    // "hello world!" döndüren genel statik bir fonksiyon olan `greet` yazın
+    public static String greet() {
+        return "hello world!";
+    }
+}
+
+
+
+
+/*
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+  @Test
+  public void testHelloWorld() throws Exception {
+    assertEquals("hello world!", HelloWorld.greet());
+  }
+}
+*/
