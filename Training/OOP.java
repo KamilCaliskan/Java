f5930ac17415ebee9152746f3e0ebe2154100bbf
Kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-06-19 16:26:47 +0200

    Update OOP.java

--- Training/OOP.java
+++ Training/OOP.java
@@ -63,3 +63,23 @@ public class Main {
         System.out.println("Rectangle Perimeter: " + rectangle.perimeter());
     }
 }
+
+
+/*
+
+1. **Soyut Sınıf (Abstract Class)**:
+   - `Shape` sınıfı, ortak özellikleri ve metodları tanımlar.
+   Soyut metodlar (`area` ve `perimeter`), bu sınıfı miras alan alt sınıflar 
+   tarafından uygulanmak zorundadır
+
+2. **Alt Sınıflar (Subclasses)**:
+   - `Circle` ve `Rectangle` sınıfları, `Shape` sınıfından türetilmiştir ve
+   `area` ile `perimeter` metodlarını kendi özelliklerine göre uygular
+
+3. **Ana Sınıf (Main Class)**:
+   - `Main` sınıfı, `Shape` tipinde `Circle` ve `Rectangle` nesneleri oluşturur ve 
+   bu nesnelerin alan ve çevrelerini hesaplayarak ekrana yazdırır.
+
+Bu yapı, nesne yönelimli programlama (OOP) prensiplerine uygun olarak şekil 
+hiyerarşisini ve bu hiyerarşiyi kullanan bir ana programı göstermektedir
+*/
