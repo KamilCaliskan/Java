d4e70cbbf42b17e781128cc1c982e07d04a646cd
Kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-06-19 16:03:22 +0200

    Update loops.java

--- Training/loops.java
+++ Training/loops.java
@@ -21,3 +21,19 @@ class Solution{
         in.close();
     }
 }
+
+
+
+/*
+n a, b ve n değerleriyle, a + b * (2^0 + 2^1 + 2^2 + ... + 2^(n-1))
+
+in.close(); ile Scanner nesnesi kapatılır. 
+Bu, kaynak sızıntılarını önlemek için iyi bir uygulamadır
+
+
+sum += b * (1 << j); ifadesi, serinin bir sonraki terimini hesaplar
+1 << j ifadesi, 2^j anlamına gelir.
+Örneğin, j 0 ise 1 << 0 -> 2^0 = 1
+j=1 ise 1 << 1 -> 2^1 = 2
+j=2 ise 1 << 2 -> 2^2 = 4 
+*/
